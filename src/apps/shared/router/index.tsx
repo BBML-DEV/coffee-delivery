33af0d1cd53f61976f76a063d3839afcba14946b
Paulo Vitor <pv.marques2010@hotmail.com>
2023-06-23 11:04:09 -0300

    feat: adicionando a página de pedidos confirmados nas rotas

--- src/apps/shared/router/index.tsx
+++ src/apps/shared/router/index.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from '../../pages/Home'
 import { CheckOut } from '../../pages/Checkout'
 import { DefaultLayout } from '../../layout'
+import { Confirmed } from '../../pages/Confirmed'
 
 export const Router = () => {
   return (
@@ -9,6 +10,7 @@ export const Router = () => {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/Checkout" element={<CheckOut />} />
+        <Route path="/Confirmed" element={<Confirmed />} />
       </Route>
     </Routes>
   )
